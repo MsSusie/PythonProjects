15e734b86093ac4c468c2c508d1700df8b4ce5ea
MsSusie <77749909+MsSusie@users.noreply.github.com>
2021-05-20 09:54:06 -0400

    Add files via upload
    From Automate the Boring Stuff - self practice projects: Program that walks through the folder tree and searches for files with certain extensions (in this case, .txt). Once this file is found, copy it and move it to a new destination.

---
+++ selectiveCopy.py
@@ -0,0 +1,44 @@
+#! python 3
+#  program that walks through folder tree and searches for files with certain file ext (.txt)
+#  copy the files from source into destination
+
+import shutil, os, glob
+from pathlib import Path
+
+def fileFinder(folder, destination):
+
+    os.chdir(folder)
+
+    # traverse the folder tree
+    # copy files and move to new folder shutil.copy()
+    for folders, subFolders, filenames in os.walk(folder):
+        for folder in folders:
+            for subFolder in subFolders:
+                for filename in filenames:
+                    if filename.endswith('.txt'):
+                        shutil.copy(filename, destination)
+    print("All .txt files copied over.")
+
+
+while True:
+    # get input from user for source filepath
+    sourceInput = input("Give a source filepath. ")
+
+    if not os.path.exists(sourceInput):
+        print("Please put in a valid absolute filepath. ")
+        continue
+
+    destinationInput = input("Give a destination filepath. ")
+    if not os.path.exists(destinationInput):
+        os.mkdir(destinationInput)
+
+    # call the function
+    fileFinder(sourceInput,destinationInput)
+    break
+
+
+
+
+
+
+
