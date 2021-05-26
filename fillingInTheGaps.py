7e9dcff33e86412e66082605dc5f54de19daff29
MsSusie <77749909+MsSusie@users.noreply.github.com>
2021-05-26 14:18:34 -0400

    Issues with Renaming Files after a Regex Search
    Project from "Automate the Boring Stuff with Python" Chapter 9. Exercise is to:  "Write a program that finds all files with a given prefix, such as spam001.txt, spam002.txt, and so on, in a single folder and locates any gaps in the numbering (such as if there is a spam001.txt and spam003.txt but no spam002.txt). Have the program rename all the later files to close this gap."
    
    The program worked fine up until I added code at lines 40 and after. I am trying to rename the files based on whether a gap occurs. In my dummy folder on my computer, I have a folder with sample files such as "eggs001.txt, eggs002.txt, eggs003.txt, notNumberedFile.txt, eggs005.txt" so I know that there is a gap between eggs003.txt and eggs004.txt. I'm not sure if the way I am renaming files is wrong or if there is a better way of doing this.

---
+++ fillingInTheGaps.py
@@ -0,0 +1,53 @@
+# python 3
+# program that finds all files with a given prefix eg. spam001.txt, spam002.txt, spam004.txt etc and
+# locates any gaps in the numbering eg. missing spam003.txt when the file jumps to spam004.txt
+# program should rename all the later files to close the gaps
+
+import re, os, glob, shutil
+from pathlib import Path
+
+
+while True:
+
+    userPathInput = input("Where is the file path that you want to check? ")
+    filePath = os.chdir(userPathInput)
+
+    p = Path(userPathInput)
+    pathExists = os.path.exists(p)
+
+    if not pathExists:
+        print("That file path does not exist. Try again.")
+        print('')
+        continue
+    else:
+        for folders, subfolders, files in sorted(os.walk(p)):               # traverse each file in folder and reads filename
+            for file in files:
+
+                fileRegex = re.compile(r'(\w{1,})(\d{1,})(.\w+)')
+                fileMO = fileRegex.search(file)
+
+                if fileMO == None:
+                    print("Not a match.")
+
+                else:
+                    print(fileMO.group())
+
+                    prefix = fileMO.group(1)  # if file 'eggs001.txt' then prefix = eggs00
+                    numberInFile = int(fileMO.group(2))     # if file 'eggs001.txt' then numberInFile = 1
+                    suffix = fileMO.group(3) # extension of file
+                    counter = numberInFile # use this to check against the file number pulled from fileMO.group(2) to check for gaps, starts at whatever the smallest file number is
+
+                    for i in range(len(glob.glob('*[0-9].txt'))):
+
+                        if numberInFile != counter:  # finds the gap in the numbering by comparing file number against counter
+                            renamedFile = prefix + str(counter) + suffix    # rename the file by putting together the pieces
+                            if not os.path.exists(file):
+                                print(renamedFile)                              # test to see if it renamed correctly
+                                shutil.move(os.path.abspath(file), os.path.abspath(renamedFile))
+                                counter += 1
+
+
+
+        break
+
+
